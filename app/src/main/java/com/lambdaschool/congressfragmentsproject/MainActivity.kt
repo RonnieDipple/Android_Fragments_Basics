fab3d60f6c16b92e2931fb439fa3a989fa581e81
ronniedipple <ronniedipple@gmail.com>
2019-08-20 23:26:56 -0500

    Added small layout, project incomplete

--- app/src/main/java/com/lambdaschool/congressfragmentsproject/MainActivity.kt
+++ app/src/main/java/com/lambdaschool/congressfragmentsproject/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.lambdaschool.congressfragmentsproject
 
 import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.lambdaschool.congressfragmentsproject.api.CongressDao
 import com.lambdaschool.congressfragmentsproject.api.CongresspersonDetails
 import com.lambdaschool.congressfragmentsproject.api.CongresspersonOverview
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_congresspersonoverview.*
 import java.util.ArrayList
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),CongresspersonOverviewFragment.OnListFragmentInteractionListener {
+
+    override fun onListFragmentInteraction(item: CongresspersonOverview?) {
+        val fragment = DetailsFragment()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +30,12 @@ class MainActivity : AppCompatActivity() {
 
         // get congressperson portrait
         val image: Bitmap? = allMembers[0].id?.let { CongressDao.getImage(it) }
+
+
+        val bundle = Bundle()
+
+
+
+
     }
 }
